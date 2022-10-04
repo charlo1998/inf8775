113dd9f772ae9e5bcf30ee54e35a9d9856b073a2
SpongeyBob <farid.elfakhrypolymtl.ca>
2022-10-04 15:28:39 -0400

    fixes

--- TP1/graph_plotting.py
+++ TP1/graph_plotting.py
@@ -3,17 +3,27 @@ import matplotlib.pyplot as plt
 from numpy import poly1d, polyfit
   
 # x axis values
-x = [250,  1250, 6250, 31250,156250]
-# corresponding y axis values
-y_brute = [18003463.745117188,430096387.8631592,10510353326.797485,263434805631.6376,6582784409523.01]
-y_recur = [1000165.9393310547 ,8000850.677490234,94021081.92443848,769171714.7827148,5304196357.727051]
-y_seuil = [ 1000165.9393310547 , 6001472.473144531 , 60011863.708496094 , 582129716.873169 , 3918689012.527466]
+x = []
+y_brute = []
+y_recur = []
+y_seuil = []
 
+with open('analyse.csv', 'r') as infile:
+    infile.readline()
+    for line in infile:
+        nums= line.split(',')
+        x.append(float(nums[0].strip()))
+        y_brute.append(float(nums[1].strip())*1000)
+        y_recur.append(float(nums[2].strip())*1000)
+        y_seuil.append(float(nums[3].strip())*1000)
+
+
+        
 # setting up the graph
 plt.xlabel('x - nombre de points (unite)')
 plt.ylabel("y - temps d'execution (us)")
-plt.xscale('log')
-plt.yscale('log')
+# plt.xscale('log')
+# plt.yscale('log')
 
 # plotting the points 
 brute_points = plt.scatter(x, y_brute)
@@ -22,19 +32,19 @@ seuil_points = plt.scatter(x,y_seuil)
 
 
 #courbe tendance
-brute_tendance = polyfit(x,y_brute, 3)
+brute_tendance = polyfit(x,y_brute, 2)
 brute_func = poly1d(brute_tendance)
 print(brute_func)
-recursif_tendance = polyfit(x,y_recur,3)
+recursif_tendance = polyfit(x,y_recur,1)
 recur_func = poly1d(recursif_tendance)
 print(recur_func)
-seuil_tendance = polyfit(x,y_seuil,3)
+seuil_tendance = polyfit(x,y_seuil,1)
 seuil_func = poly1d(seuil_tendance)
 print(seuil_func)
 plt.plot(x,brute_func(x), label = "tendance brute")
 plt.plot(x,recur_func(x), label = "DpR sans seuil")
 plt.plot(x,seuil_func(x), label = "DpR avec Seuil")
-plt.legend((brute_points, recursif_points,seuil_points),("force brute", "recursif sans seuil","recusrif avec seuil"), loc="upper left")
+# plt.legend((brute_points, recursif_points,seuil_points),("force brute", "recursif sans seuil","recusrif avec seuil"), loc="upper left")
 # giving a title to my graph
 plt.title('Time usage per amount of points')
   

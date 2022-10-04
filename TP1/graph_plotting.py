5065a18c7189ceda618856a4ed857ae822dc89f0
SpongeyBob <farid.elfakhrypolymtl.ca>
2022-10-04 17:17:18 -0400

    esthetic fix

--- TP1/graph_plotting.py
+++ TP1/graph_plotting.py
@@ -8,11 +8,14 @@ y_brute = []
 y_recur = []
 y_seuil = []
 
+#lire le fichier generer. 
 with open('analyse.csv', 'r') as infile:
+    #ignorer la premiere ligne
     infile.readline()
     for line in infile:
         nums= line.split(',')
         x.append(float(nums[0].strip()))
+        # le facteur multiplicatif est pour transformer en us
         y_brute.append(float(nums[1].strip())*1000)
         y_recur.append(float(nums[2].strip())*1000)
         y_seuil.append(float(nums[3].strip())*1000)
@@ -22,8 +25,8 @@ with open('analyse.csv', 'r') as infile:
 # setting up the graph
 plt.xlabel('x - nombre de points (unite)')
 plt.ylabel("y - temps d'execution (us)")
-# plt.xscale('log')
-# plt.yscale('log')
+plt.xscale('log')
+plt.yscale('log')
 
 # plotting the points 
 brute_points = plt.scatter(x, y_brute)
@@ -31,22 +34,27 @@ recursif_points = plt.scatter(x,y_recur)
 seuil_points = plt.scatter(x,y_seuil)
 
 
-#courbe tendance
+#calcul des courbes
 brute_tendance = polyfit(x,y_brute, 2)
 brute_func = poly1d(brute_tendance)
 print(brute_func)
+
 recursif_tendance = polyfit(x,y_recur,1)
 recur_func = poly1d(recursif_tendance)
 print(recur_func)
+
 seuil_tendance = polyfit(x,y_seuil,1)
 seuil_func = poly1d(seuil_tendance)
 print(seuil_func)
+
+# plot it.
 plt.plot(x,brute_func(x), label = "tendance brute")
 plt.plot(x,recur_func(x), label = "DpR sans seuil")
 plt.plot(x,seuil_func(x), label = "DpR avec Seuil")
-# plt.legend((brute_points, recursif_points,seuil_points),("force brute", "recursif sans seuil","recusrif avec seuil"), loc="upper left")
-# giving a title to my graph
+plt.legend((brute_points, recursif_points,seuil_points),("force brute", "recursif sans seuil","recusrif avec seuil"), loc="upper left")
+
+# un beau petit titre
 plt.title('Time usage per amount of points')
-  
-# function to show the plot
+
+#afficher
 plt.show()

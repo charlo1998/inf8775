e17f8e7aa34c185ce91d023d1bac11081e8f1d10
Farid <farid.elfakhry@gmail.com>
2022-11-10 14:15:28 -0500

    try #67 on heuristic

--- TP2/heuristic.cpp
+++ TP2/heuristic.cpp
@@ -1,38 +1,34 @@
 #include "heuristic.hpp"
-int calculer_qtty_disponible(vector<Restaurant> restos, int N)
-{
-    int qtty = 0;
-    for (auto &resto : restos)
-    {
-        qtty += resto.qtty;
-    }
-    return N - qtty;
-}
-bool is_in(const Restaurant resto, const vector<Restaurant> restos)
-{
-    for (auto &obj : restos)
-    {
-        if (resto == obj)
-        {
-            return true;
-        }
-    }
-    return false;
-}
+
+/**
+ * @
+*/
 vector<Restaurant> start_heuristic(vector<Restaurant> restos, int N)
 {
     float revenu;
-    vector<Restaurant> greed = greedy(restos, N, revenu);
-    cout << "greed" << endl
+    vector<Restaurant> solution = greedy(restos, N, revenu);
+    cout << "solution avec greed" << endl
          << "******************" << endl;
-    for (auto &resto : greed)
+    for (auto &resto : solution)
         resto.display();
     cout << "******************" << endl;
-    return heuristic_local(greed, restos, N);
+    int swap_count;
+    int i =0;
+    // do
+    // {
+    //     swap_count = heuristic_local(solution, restos, N);
+    //     cout << "essai #" << ++i << "swap count: " << swap_count<<endl;
+    // } while (swap_count > 0);
+    swap_count = heuristic_local(solution, restos, N);
+    return solution;
 }
-vector<Restaurant> heuristic_local(vector<Restaurant> solution, vector<Restaurant> restos, int N)
+
+
+int heuristic_local(vector<Restaurant> &solution, vector<Restaurant> restos, int N)
 {
-    vector<vector<Restaurant>> combinaison_solution = combinaison(2, solution);
+    // combien de modification on etait apporté
+    int swap_count = 0;
+    vector<vector<Restaurant>> combinaison_solution = combinaison(solution);
     // minimiser le nbr de resto
     vector<Restaurant> restos_valides;
     for (auto &resto : restos)
@@ -42,13 +38,14 @@ vector<Restaurant> heuristic_local(vector<Restaurant> solution, vector<Restauran
             restos_valides.push_back(resto);
         }
     }
-    vector<vector<Restaurant>> combinaison_resto = combinaison(2, restos_valides);
+    vector<vector<Restaurant>> combinaison_resto = combinaison( restos_valides);
     for (auto i_sol = combinaison_solution.begin(); i_sol != combinaison_solution.end(); ++i_sol)
     {
+        // booleen qui est vrai si un candidat apporte une amelioration a la solution et donc doit etre swapped
+        bool isBetter = false;
         // calculer N restant si i_sol est enlever:
         int N_restant = calculer_qtty_disponible(solution, N);
         // trouver le meilleur candidat
-        int delta_revenue = 0;
         vector<Restaurant> candidat = *i_sol;
         for (auto &combinaison : combinaison_resto)
         {
@@ -56,26 +53,33 @@ vector<Restaurant> heuristic_local(vector<Restaurant> solution, vector<Restauran
             if (N_restant >= 0)
             {
                 int revenue_diff = compare_revenue(candidat, combinaison);
-                if (revenue_diff > delta_revenue)
+                if (revenue_diff > 0)
                 {
-                    delta_revenue = revenue_diff;
+                    isBetter = true;
                     candidat = combinaison;
                 }
             }
         }
-        // swap 2 combinaison
-    }
-    return solution;
-}
-void swap(vector<Restaurant> container, vector<Restaurant> old, vector<Restaurant> current)
-{
-    for (auto &it = container.begin(); it != container.end(); ++it)
-    {
-        if(is_in(*it, old)){
-            remove(begin(container), end(container), it);
+        // swap 2 if there is a gain
+        if (isBetter)
+        {
+            swap_vectors(solution, *i_sol, candidat);
+            swap_count++;
         }
     }
-    for(auto& resto : current){
+    return swap_count;
+}
+void swap_vectors(vector<Restaurant> container, vector<Restaurant> old, vector<Restaurant> current)
+{
+    for (auto it = container.begin(); it != container.end(); ++it)
+    {
+        if (is_in(*it, old))
+        {
+            remove(begin(container), end(container), *it);
+        }
+    }
+    for (auto &resto : current)
+    {
         container.push_back(resto);
     }
 }
@@ -110,7 +114,7 @@ int compare_revenue(vector<Restaurant> old, vector<Restaurant> current)
     return revenue_current - revenue_old;
 }
 // trouver touteles combinaisons possibles
-vector<vector<Restaurant>> combinaison(int n, vector<Restaurant> restos)
+vector<vector<Restaurant>> combinaison( vector<Restaurant> restos)
 {
     vector<vector<Restaurant>> combinaisons;
     for (auto &resto : restos)
@@ -133,4 +137,24 @@ vector<vector<Restaurant>> combinaison(int n, vector<Restaurant> restos)
         }
     }
     return combinaisons;
+}
+int calculer_qtty_disponible(vector<Restaurant> restos, int N)
+{
+    int qtty = 0;
+    for (auto &resto : restos)
+    {
+        qtty += resto.qtty;
+    }
+    return N - qtty;
+}
+bool is_in(const Restaurant resto, const vector<Restaurant> restos)
+{
+    for (auto &obj : restos)
+    {
+        if (resto == obj)
+        {
+            return true;
+        }
+    }
+    return false;
 }
--- TP2/heuristic.hpp
+++ TP2/heuristic.hpp
@@ -1,5 +1,48 @@
 #include "restaurant.hpp"
 #include "glouton.hpp"
-
+/**
+ * fonction qui execute l'algorithm heuristic local
+ * @param restos vecteur contenant tout les restaurant
+ * @param N quantite maximal
+ * @return vecteurs contenant la solution
+*/
 vector<Restaurant> start_heuristic(vector<Restaurant> restos, int N);
-vector<Restaurant> heuristic_local(vector<Restaurant> solution, vector<Restaurant> restos, int N);
+/**
+ * fonction qui execute une iteration.
+ * @param solution la solution actuel (sera modifier)
+ * @param restos vecteur contenant les restos
+ * @param N la quantite maximal
+ * @return le nombre de modifications effectue a la solution actuel
+ */
+int heuristic_local(vector<Restaurant>& solution, vector<Restaurant> restos, int N);
+/**
+ * fonction qui remplace dans un vecteur le contenue d'un deuxieme par le contenue du troisieme
+ * @param container le vecteur sur lequel onv eut effectuer le changement
+ * @param old le vecteur contenant les element a enlever
+ * @param current le vecteur qui contient les element a mettre
+*/
+void swap_vectors(vector<Restaurant> container, vector<Restaurant> old, vector<Restaurant> current);
+/**
+ * fonction qui calcule la difference de la somme des quantite des restaurant dans les vecteur
+ * @param old premier vecteur
+ * @param current deuxieme vecteur
+ * @return  >0 si qtty current > qtty old
+*/
+int compare_qtty(vector<Restaurant> old, vector<Restaurant> current);
+/**
+fonction qui calcule la difference de la somme des revenues des restaurant dans les vecteur
+ * @param old premier vecteur
+ * @param current deuxieme vecteur
+ * @return  >0 si revenu current > revenu old
+*/
+int compare_revenue(vector<Restaurant> old, vector<Restaurant> current);
+/**
+ * fonction qui retourne les combinaise de 1 et de 2 du vecteur
+ * 1C(restos.size) + 2C(vecteur.size)
+ * @param restos le vecteur de restos
+ * @return la liste de ttes les combinaisons
+*/
+vector<vector<Restaurant>> combinaison( vector<Restaurant> restos);
+
+int calculer_qtty_disponible(vector<Restaurant> restos, int N);
+bool is_in(const Restaurant resto, const vector<Restaurant> restos);

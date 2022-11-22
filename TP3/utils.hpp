27f94f8970348cb3c63b402217b1bcb1736ddfca
farid <farid.elfakhry@gmail.com>
2022-11-21 19:09:07 -0500

    util functions

---
+++ TP3/utils.hpp
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include <math.h>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+
+typedef struct {
+    int x,y;
+    uint8_t voteCount;
+} Municipalite;
+
+typedef struct {
+    int m_count;
+    Municipalite** municipalities;
+    bool isVotingGreen;
+} Circonscription;
+
+int distance(const Municipalite& m1, const Municipalite &m2);
+
+std::vector<Municipalite> read_input_data(const std::string& filename);
+
+std::ostream& operator<<(std::ostream& out, const Municipalite &mun);
+std::ostream& operator<<(std::ostream& out, const Circonscription &circ);

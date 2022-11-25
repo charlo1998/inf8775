f38578b377d3c687b904fce620984b3f7f2d69eb
charlo1998 <charles19982009@hotmail.com>
2022-11-25 15:53:09 +0000

    trivial sol

--- TP3/heuristique.cpp
+++ TP3/heuristique.cpp
@@ -1,6 +1,16 @@
 #include "heuristique.hpp"
 void generate_initial_solution(std::vector<Municipalite> &munis, int n_circ, int dist_max, int x_size, int y_size){
     
+    std::vector<Circonscription> solution;
+    for(int i = 0; i<3; i++){
+        Circonscription circ;
+
+        for(int j =0; j<5; j++){
+            circ.addMunicipalite(municipalities[5*i+j], 5);
+        }
+        solution.push_back(circ);
+        
+    }
 }
 
 void heuristique(std::vector<Municipalite> &munis){

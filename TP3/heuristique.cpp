483b3a0523b6ea8ec21aa70bae9a1a83f3304c3b
charlo1998 <charles19982009@hotmail.com>
2022-11-29 03:28:22 +0000

    removed some warnings

--- TP3/heuristique.cpp
+++ TP3/heuristique.cpp
@@ -4,7 +4,7 @@ std::vector<Circonscription> generate_initial_solution(std::vector<Municipalite>
     std::vector<Circonscription> solution;
     std::vector<Municipalite> muniStart;
     int total = 0;
-    for(int i=0; i<munis.size(); i++){
+    for(int i=0; i<static_cast<int>(munis.size()); i++){
         total += munis[i].votes;
     }
     std::cout << "total amount of votes: " << total << endl;
@@ -16,7 +16,6 @@ std::vector<Circonscription> generate_initial_solution(std::vector<Municipalite>
 
     //create starting points for all the circ
     int idx = 0;
-    float counter=0;
 
     //rectangular tessellation TODO
     float x = sqrt(x_size/float(y_size)*float(n_circ));
@@ -48,7 +47,7 @@ std::vector<Circonscription> generate_initial_solution(std::vector<Municipalite>
         for(int i = y-1; i >= 0; i--) {
             for(int j = x-1; j >= 0; j--) {
                 if (m < n_circ){
-                    int idx = round(x_jump*j) + round(y_jump*i)*x_size;
+                    idx = round(x_jump*j) + round(y_jump*i)*x_size;
                     //std::cout << "idx: " << idx << endl;
                     Circonscription circ(m);
                     circ.addMunicipalite(munis[idx], dist_max, max_size);
@@ -76,7 +75,7 @@ std::vector<Circonscription> generate_initial_solution(std::vector<Municipalite>
             if (!full_circs){
                 full_circs = true;
                 for (int i = 0; i<n_circ; i++){
-                    if(solution[i].getCount() < min_size){
+                    if(static_cast<int>(solution[i].getCount()) < min_size){
                         full_circs = false;
                         int dist = solution[i].distance(municipalite, muniStart[i]);
                         //std::cout << "i: " << i  << " muni: " << municipalite << "circ start: " << muniStart[i] << " dist: " << dist << endl;
@@ -95,7 +94,7 @@ std::vector<Circonscription> generate_initial_solution(std::vector<Municipalite>
                 }
             } else { // didn't find a circ with less than min_size munis now add to max size
                 for (int i = 0; i<n_circ; i++){
-                    if(solution[i].getCount() < max_size){
+                    if(static_cast<int>(solution[i].getCount()) < max_size){
                         int dist = solution[i].distance(municipalite, muniStart[i]);
                         if ( dist < dist_min) {
                             dist_min = dist;
@@ -146,7 +145,7 @@ std::vector<Circonscription> generate_initial_solution(std::vector<Municipalite>
         }
         //first check if we can add one of the remaining munis
         bool success = false;
-        for (int i = 0; i<muniOrphelines.size(); i++){
+        for (int i = 0; i<static_cast<int>(muniOrphelines.size()); i++){
             //we need to assign from the real set, so find whichs muni this correpsonds to in munis
             int x = muniOrphelines[i].x;
             int y = muniOrphelines[i].y;
@@ -163,7 +162,7 @@ std::vector<Circonscription> generate_initial_solution(std::vector<Municipalite>
             //solution[smallest_idx].print();
             Municipalite stolen = solution[smallest_idx].stealNeighbor(solution, munis, x_size, y_size, dist_max, max_size);
             if(stolen.i_circ == -1){
-                for(int i = 0; i < muniOrphelines.size(); i++){
+                for(int i = 0; i < static_cast<int>(muniOrphelines.size()); i++){
                     if(stolen == muniOrphelines[i]) {
                         muniOrphelines[i] = muniOrphelines.back();
                         muniOrphelines.pop_back();
@@ -174,36 +173,36 @@ std::vector<Circonscription> generate_initial_solution(std::vector<Municipalite>
         }
     }
 
-    
+
+    std::cout << "creating circonscription graph" << endl;    
     //creation du graphe des circonscriptions  n*m
-    // for(int i = 0; i < y_size; i++) {
-    //     for(int j = 0; j < floor(x_size/2.0f)+x_size%2; j++) {
-    //         int idx = (2*j) + (i)*x_size + (1-i%2);
-    //         //generation voisinage de munis[idx]
-    //         for(int dx=-1; dx<2;dx++) {
-    //             for(int dy=-1; dy<2;dy++) {
-    //                 int newX = munis[idx].x + dx;
-    //                 int newY = munis[idx].y + dy;
-    //                  //make sure we are still within bounds
-    //                 if (newX >= 0 && newX < x_size && newY >= 0 && newY < y_size) {
-    //                     int neighbour_circ = munis[x_size*newY + newX].i_circ;
-    //                     int current_circ = munis[idx].i_circ;
-    //                      //if either muni is unassigned, skip
-    //                     if (current_circ != -1 && neighbour_circ != -1){
-    //                         if (neighbour_circ != current_circ){ //found a connection!
-    //                              // if not already there, add it to the neighbours
-    //                             if(!solution[current_circ].isNeighbour(neighbour_circ)) { //opération baromètre linéaire en m
-    //                                 solution[current_circ].voisins.push_back(neighbour_circ);
-    //                                 solution[neighbour_circ].voisins.push_back(current_circ);
-    //                             }
-    //                         }
-    //                     }
-                        
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
+    for(int i = 0; i < y_size; i++) {
+        for(int j = 0; j < floor(x_size/2.0f)+x_size%2; j++) {
+            int idx = (2*j) + (i)*x_size + (1-i%2);
+            //generation voisinage de munis[idx]
+            for(int dx=-1; dx<2;dx++) {
+                for(int dy=-1; dy<2;dy++) {
+                    int newX = munis[idx].x + dx;
+                    int newY = munis[idx].y + dy;
+                     //make sure we are still within bounds
+                    if (newX >= 0 && newX < x_size && newY >= 0 && newY < y_size) {
+                        int neighbour_circ = munis[x_size*newY + newX].i_circ;
+                        int current_circ = munis[idx].i_circ;
+                         //if either muni is unassigned, skip
+                        if (current_circ != -1 && neighbour_circ != -1){
+                            if (neighbour_circ != current_circ){ //found a connection!
+                                 // if not already there, add it to the neighbours
+                                if(!solution[current_circ].isNeighbour(neighbour_circ)) { //opération baromètre linéaire en m
+                                    solution[current_circ].voisins.push_back(neighbour_circ);
+                                    solution[neighbour_circ].voisins.push_back(current_circ);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
     // for(auto &circ : solution){
     //     std::cout << "Circ: " << circ.id << " Neighbours: ";
     //     for(int &voisin: circ.voisins){
